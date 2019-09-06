ce8bb386c833add4120895633289cf017efe2b06
TakumaNakagame <5129906+TakumaNakagame@users.noreply.github.com>
2019-09-06 12:28:56 +0900

    :heavy_plus_sign: add bootstrapvue

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
@@ -10,4 +14,5 @@ export default function (Vue, { router, head, isClient }) {
     src: 'https://platform.twitter.com/widgets.js',
     body: true
   })
+  Vue.use(BootstrapVue)
 }
