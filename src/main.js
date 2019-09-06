d9818b28981391026a9a2864ac6b27c825f046fe
TakumaNakagame <5129906+TakumaNakagame@users.noreply.github.com>
2019-09-06 11:57:24 +0900

    :+1: add social button

--- src/main.js
+++ src/main.js
@@ -6,4 +6,8 @@ import DefaultLayout from '~/layouts/Default.vue'
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+  head.script.push({
+    src: 'https://platform.twitter.com/widgets.js',
+    body: true
+  })
 }
--- src/templates/Episode.vue
+++ src/templates/Episode.vue
@@ -3,11 +3,21 @@
     <h1>{{ $page.episode.slug }} - {{ $page.episode.title }}</h1>
     <div class="item-title" v-html="$page.episode.date" />
     <div class="content" v-html="$page.episode.content" />
+    <span class="social-twitter">
+      <a href="https://twitter.com/tanoshii_works?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @tanoshii_works</a>
+    </span>
+    <span class="social-twitter">
+      <a href="https://twitter.com/intent/tweet?button_hashtag=たのしいWorks&ref_src=twsrc%5Etfw" class="twitter-hashtag-button" data-show-count="false">Tweet #たのしいWorks</a>
+    </span>
     <div class="player" v-html="$page.episode.player" />
-    <!-- <h2>エピソード内のリンク</h2>
+    <h2>ネタ帳</h2>
     <ul v-for="link in $page.episode.links" :key="link.title">
-      <li><a href="link.url" target="_blank"><span v-html="link.title" /></a></li>
-    </ul> -->
+      <li>
+        <a :href="link.url" target="_blank">
+          <span v-html="link.title" />
+        </a>
+      </li>
+    </ul>
   </Layout>
 </template>
 
@@ -26,3 +36,9 @@
     }
   }
 </page-query>
+
+<style scoped>
+.social-twitter {
+  margin: 10px;
+}
+</style>
